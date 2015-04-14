eb22277f170b2acc533e2b6ffb1e690d62de766f
Lich <longzy2013@gmail.com>
2015-04-14 13:45:30 +0800

    db

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -13,7 +13,7 @@ return array(
     */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
     'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  'test', // 数据库名
+    'DB_NAME'               =>  'date', // 数据库名
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  '',    // 密码
     'DB_PORT'               =>  '3306',     // 端口
