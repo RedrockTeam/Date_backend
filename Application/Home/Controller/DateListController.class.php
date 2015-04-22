d436a7210d22b9de414774a5746c817fb00f23be
Lich <longzy2013@gmail.com>
2015-04-22 12:13:28 +0800

    change

--- Application/Home/Controller/DateListController.class.php
+++ Application/Home/Controller/DateListController.class.php
@@ -7,11 +7,14 @@ use Think\Controller;
 class DateListController extends Controller {
     //获取广告位广告
     public function getList () {
-        if(!IS_POST)
-            $this->ajaxReturn('获取方式错误!');
-
+        if (!IS_POST) {
+            $data['info'] = '获取方式错误!';
+            $data['status'] = 403;
+            $this->ajaxReturn($data);
+        }
         $list = new DateModel();
         $data = $list->getInfo();
+        $data['status'] = 200;
         header("Access-Control-Allow-Origin: *");
         $this->ajaxReturn($data);
     }
