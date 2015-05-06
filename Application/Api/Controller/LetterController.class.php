245a9da7bde8abed91b4e51c279433e85f1d0b6c
Lich <longzy2013@gmail.com>
2015-05-06 18:40:54 +0800

    letter ok?

--- Application/Api/Controller/LetterController.class.php
+++ Application/Api/Controller/LetterController.class.php
@@ -4,14 +4,34 @@ use Api\Model\LetterModel;
 use Think\Controller;
 class LetterController extends BaseController {
     //获取私信
-    public function getLetter (){
-        $data = I('post.');
+    public function getLetter () {
+        $input = I('post.');
         $letter = new LetterModel();
         $data = [
-            'data' => $letter->letter($data['uid'], $data['page'], $data['size']),
+            'data' => $letter->letter($input['uid'], $input['page'], $input['size']),
             'status' => 200,
             'info' => '请求成功',
             ];
         $this->ajaxReturn($data);
+    }
+
+    //检查是否有未读私信
+    public function letterStatus () {
+        $input = I('post.');
+        $letter = new LetterModel();
+        $data = [
+            'status' => 200,
+            'letter' => $letter->letterStatus($input['uid']),
+        ];
+        $this->ajaxReturn($data);
+
+    }
+
+    //私信方式接受/拒绝
+    public function dateAction () {
+        $input = I('post.');
+        $common = new CommonController();
+        $result = $common->dateAction($input['to_id'], $input['date_id'], $input['action']);
+        $this->ajaxReturn($result);
     }
 }
