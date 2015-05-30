b7f85dccdb7c0bbd18b9e14f316a519230d54a79
user <495871401>
2015-05-30 14:34:09 +0800

    后台

--- Application/Home/Common/function.php
+++ Application/Home/Common/function.php
@@ -14,3 +14,22 @@ function checkLogin(){
 function toPaw($origin){
     return md5(substr(md5("hongyan".$origin),3,14)."redrock");
 }
+
+function checkLevel($levelNeed=2){
+    if(checkLogin()){
+        if(session('userLevel') < $levelNeed){
+            echo '<script>alert("权限不够!");history.go(-1);</script>';
+            exit();
+        }
+    }
+}
+
+function login($post){
+
+    $password = toPaw($post['password']);
+    $right_pass = toPaw($this->userIfo[$post['name']]['password']);
+    if($password == $right_pass ){
+        session('loginUser',$post['name']);
+        session('userLevel',$post['level']);
+    } else    $this->error('密码错误');
+}
