e3587f6b7d527528c6f8111d30f7055c2ce3e6d6
Lich <longzy2013@gmail.com>
2015-06-10 20:19:44 +0800

    debug

--- Application/Api/Model/LetterModel.class.php
+++ Application/Api/Model/LetterModel.class.php
@@ -38,18 +38,13 @@ class LetterModel extends Model {
     public function detailLetter($uid, $letter_id){
         $map = [
             'letter.id' => $letter_id,
-            'letter.to' => $uid
+            'letter.to' => $uid//to_id
         ];
-        $temp = $this->where($map)->find();
-        $map2 = [
-            'user_date.user_id' => $temp['from'],
-        ];
+
         $data = $this->where($map)
                      ->join('JOIN users ON letter.from = users.id')
-                     ->join('JOIN user_date ON letter.date_id = user_date.date_id')
-                     ->where($map2)
                      ->join('JOIN date ON letter.date_id = date.id')
-                     ->field('letter.id as letter_id, users.id as user_id, users.nickname as user_name, users.signature as user_signature, users.head as user_avatar, users.gender as user_gender, letter.content as content, letter.date_id as date_id, letter.status as letter_status, user_date.status as user_date_status, date.title')
+                     ->field('letter.id as letter_id, users.id as user_id, users.nickname as user_name, users.signature as user_signature, users.head as user_avatar, users.gender as user_gender, letter.content as content, letter.date_id as date_id, letter.status as letter_status, letter.type as user_date_status, date.title')
                      ->find();
         return $data;
     }
